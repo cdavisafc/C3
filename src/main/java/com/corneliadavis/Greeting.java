ece4f48d55a8e6f51d03303b0a10e541f77ea9d5
Cornelia Davis <cornelia@corneliadavis.com>
2016-12-28 21:35:32 -0800

    Add model class

---
+++ src/main/java/com/corneliadavis/Greeting.java
@@ -0,0 +1,26 @@
+package com.corneliadavis;
+
+/**
+ * Created by cdavis on 12/28/16.
+ */
+public class Greeting {
+
+    private String greeting;
+    private String specialization;
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+}
