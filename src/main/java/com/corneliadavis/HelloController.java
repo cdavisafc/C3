2c649b31442fb5d78b41b38922c910fd1713074f
Cornelia Davis <cornelia@corneliadavis.com>
2017-01-02 19:52:00 -0800

    Move env var access to property file

--- src/main/java/com/corneliadavis/HelloController.java
+++ src/main/java/com/corneliadavis/HelloController.java
@@ -1,22 +1,20 @@
 package com.corneliadavis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
 @RestController
 public class HelloController {
 
+    @Value("${specialization}")
+    private String specialization;
+
     @RequestMapping("/")
     public Greeting hello(@CookieValue(value = "userToken", required=false) String token, HttpServletResponse response) {
-
-        String specialization;
-        specialization = System.getenv("SPECIALIZATION");
-        if (specialization == null)
-            specialization = "Education"; // default specialization
 
         if (token == null)
 			response.setStatus(401);
